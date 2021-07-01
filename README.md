91934fa683aca872939f2f35d5a608716ed76b86
Md Shayon <mdshayon0@gmail.com>
2021-07-01 21:13:11 +0600

    initial plugin setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Wordpress plugin development
+
+ - [Tutorial](https://www.youtube.com/watch?v=hbJiwm5YL5Q&t=11139s), [docs](https://developer.wordpress.org/plugins/intro/)
+ - Go to wp-content -> plugins -> create a folder with unique name
+ - Here some [header requirements](https://developer.wordpress.org/plugins/plugin-basics/header-requirements/) for plugin
+ - 
---
+++ first-unique-plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/**
+ * Plugin Name: First Unique Plugin
+ * Description: A truly amazing plugin desc
+ * Version: 1.0
+ * Author: Shayon
+ * Author URI: http://twitter.com/shayon_md
+ */
+ // https://developer.wordpress.org/plugins/plugin-basics/header-requirements/
+
+ // # WE CAN NAME ANYTHING OF THIS FILE BUT FOR BEST PRACTICE MAKE THE NAME SAME AS FOLDER NAME
+ ?>
