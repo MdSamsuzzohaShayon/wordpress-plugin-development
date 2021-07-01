57a6cd25620fb85d757aa37ab4a0fc76c95c0959
Md Shayon <mdshayon0@gmail.com>
2021-07-01 21:52:11 +0600

    show customs text into the post

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
  - [Tutorial](https://www.youtube.com/watch?v=hbJiwm5YL5Q&t=11139s), [docs](https://developer.wordpress.org/plugins/intro/)
  - Go to wp-content -> plugins -> create a folder with unique name
  - Here some [header requirements](https://developer.wordpress.org/plugins/plugin-basics/header-requirements/) for plugin
- - 
+ - Check [action hooks](https://codex.wordpress.org/Plugin_API/Action_Reference) and [filter hooks](https://developer.wordpress.org/plugins/hooks/)
+ - After setup headers activate the plugin
+ - Check [all filters](https://codex.wordpress.org/Plugin_API/Filter_Reference)
+ - [Conditional functions](https://developer.wordpress.org/themes/basics/conditional-tags/)
--- first-unique-plugin.php
+++ first-unique-plugin.php
@@ -11,4 +11,22 @@
  // https://developer.wordpress.org/plugins/plugin-basics/header-requirements/
 
  // # WE CAN NAME ANYTHING OF THIS FILE BUT FOR BEST PRACTICE MAKE THE NAME SAME AS FOLDER NAME
+
+
+// WordPress offers filter hooks to allow plugins to modify various types of internal data at runtime.
+// applied to the post content retrieved from the database, prior to printing on the screen (also used in some other operations, such as trackbacks).
+add_filter("the_content", 'addToEndOfPost');
+ // THIS FUNCTION NAME SHOULD BE UNIQUE
+function addToEndOfPost($content){
+  // HERE THIS CONTENT IS POST TEXT
+  // Determines whether the query is for an existing single post. // The is_main_query() function is a conditional function that can be used to evaluate whether the current query (such as within the loop) is the “main” query (as opposed to a secondary query).
+  if (is_single() && is_main_query()) {
+      return $content . '<h1 style="color: blue;">My Name is Shayon</h1>';
+  }else if (is_page()){
+    return '<h1 style="color: blue;">This is a page</h1>' . $content ;
+  }
+    return $content;
+}
+
+
  ?>
